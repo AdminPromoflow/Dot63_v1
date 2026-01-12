a7aa1214ad3cfb4ed9bf967d14b8910a5d3a8781
Aleina Rossui <admin@promoflow.net>
2026-01-11 20:33:16 -0500

    ...

--- controller/webhook/requestAPIProductDetails.php
+++ controller/webhook/requestAPIProductDetails.php
@@ -1,13 +1,7 @@
 <?php
 //require '../../controller/assets/vendor/autoload.php';
 
-$token = $_SERVER['HTTP_X_API_TOKEN'] ?? ($_GET['token'] ?? ($_POST['token'] ?? null));
-
-if (!$token) {
-  http_response_code(401);
-  echo json_encode(["ok" => false, "error" => "Unauthorized"]);
-  exit;
-}
+header('Content-Type: application/json; charset=utf-8');
 
 echo json_encode(["ok" => true, "msg" => "Acceso permitido"]);
 exit;
