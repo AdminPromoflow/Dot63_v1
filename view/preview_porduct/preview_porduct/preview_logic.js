e1c625ab50a2bc94d689a289195f1fe054090cac
Aleina Rossui <admin@promoflow.net>
2026-03-03 21:18:57 -0500

    mensaje

--- view/preview_porduct/preview_porduct/preview_logic.js
+++ view/preview_porduct/preview_porduct/preview_logic.js
@@ -623,17 +623,13 @@ class PreviewLogic {
   }
 
   deleteImages(typeVariation) {
-  //  alert(JSON.stringify(typeVariation));
-
-    // const typeName = String(typeVariation?.type_name ?? "").trim();
-    // if (!typeName) return;
-    //
-    // const typeId = typeVariation?.type_id ?? "null";
     alert(`wrap-images-${typeVariation}`);
     const el = document.getElementById(`wrap-images-${typeVariation}`);
   //  alert("wrap-images-" + typeId);
 
-    if (el) el.remove();
+  el.innerHTML = "";
+
+    //if (el) el.remove();
   }
 
   deletePrices(typeVariation) {
