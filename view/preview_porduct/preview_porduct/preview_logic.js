a20dd2dae74a0325d6b880083bf15d9a4d573c9f
Aleina Rossui <admin@promoflow.net>
2026-03-17 22:47:29 -0400

    mensaje

--- view/preview_porduct/preview_porduct/preview_logic.js
+++ view/preview_porduct/preview_porduct/preview_logic.js
@@ -245,6 +245,7 @@ class PreviewLogic {
       }
 
       if (!Array.isArray(imagesOnlyOfType) || imagesOnlyOfType.length === 0 ) {
+        alert("No entiendo porque rayos entra acá!");
         this.renderImages(imagesOnlyOfType, typeVariation);
       }
 
