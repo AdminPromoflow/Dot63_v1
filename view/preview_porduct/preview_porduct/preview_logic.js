8e262a95b549a7c37b16738083b746d115d03b4a
Aleina Rossui <admin@promoflow.net>
2026-03-31 15:57:54 -0400

    mensaje

--- view/preview_porduct/preview_porduct/preview_logic.js
+++ view/preview_porduct/preview_porduct/preview_logic.js
@@ -272,6 +272,12 @@ class PreviewLogic {
         }
       }
 
+      const wrap_images_62 = document.getElementById("wrap-images-62");
+
+      if (wrap_images_62) {
+        wrap_images_62.remove();
+      }
+
 
       this.renderItems(itemsOnlyOfType, typeVariation);
       this.renderPrices(pricesOnlyOfType, typeVariation);
