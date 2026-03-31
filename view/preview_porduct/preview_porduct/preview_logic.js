fd5de426914afda053c1a4325ab152d6c8a34835
Aleina Rossui <admin@promoflow.net>
2026-03-31 17:36:42 -0400

    mensaje

--- view/preview_porduct/preview_porduct/preview_logic.js
+++ view/preview_porduct/preview_porduct/preview_logic.js
@@ -167,7 +167,6 @@ class PreviewLogic {
         // if (variationTypesForDelete.length) {
         //   var organizeVariationsForDelete = this.organizeVariationsForDelete(variationTypesForDelete, currentTypeId);
         // }
-        alert("Esto devería salir de primeras" + organizeVariationsForDelete);
 
         // ✅ 2) PINTAR lo nuevo
         if (childVariations.length && variationTypes.length ) {
