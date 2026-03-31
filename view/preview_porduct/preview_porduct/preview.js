82893a4f4b8bcfed66ed71aa5bd786e98aec835c
Aleina Rossui <admin@promoflow.net>
2026-03-31 16:04:38 -0400

    mensaje

--- view/preview_porduct/preview_porduct/preview.js
+++ view/preview_porduct/preview_porduct/preview.js
@@ -30,7 +30,7 @@ class PreviewPage {
     //this.bindZoomEvents();
 
     // Enables the previous arrow button
-    this.bindPrevButton();
+  //  this.bindPrevButton();
 
     // Allows only one pack size to be selected globally
     //this.setupPackSizeSelection();
@@ -61,7 +61,7 @@ class PreviewPage {
     });
 
     // Activates Back and Publish buttons
-  //  this.setupBackPublishButtons();
+    this.setupBackPublishButtons();
   }
 
   /* ============================================================================
@@ -579,37 +579,37 @@ class PreviewPage {
      Back and Publish buttons
   ============================================================================ */
 
-  // setupBackPublishButtons() {
-  //   const backBtn = document.getElementById("btn_back_edit");
-  //   const publishBtn = document.getElementById("btn_publish");
-  //
-  //   if (backBtn) {
-  //     backBtn.addEventListener("click", () => {
-  //       const url = "../../view/product_details/index.php";
-  //
-  //       const current = new URL(window.location.href);
-  //       const dest = new URL(url, current);
-  //
-  //       const sku = current.searchParams.get("sku");
-  //       const skuv = current.searchParams.get("sku_variation");
-  //
-  //       // Preserves sku and sku_variation in the destination URL
-  //       if (sku) dest.searchParams.set("sku", sku);
-  //       if (skuv) dest.searchParams.set("sku_variation", skuv);
-  //
-  //       window.location.assign(dest);
-  //     });
-  //   }
-  //
-  //   if (publishBtn) {
-  //     publishBtn.addEventListener("click", () => {
-  //       alert(
-  //         "Your configuration will now be reviewed and then approved. " +
-  //         "This page is currently under construction."
-  //       );
-  //     });
-  //   }
-  // }
+  setupBackPublishButtons() {
+    const backBtn = document.getElementById("btn_back_edit");
+    const publishBtn = document.getElementById("btn_publish");
+
+    if (backBtn) {
+      backBtn.addEventListener("click", () => {
+        const url = "../../view/product_details/index.php";
+
+        const current = new URL(window.location.href);
+        const dest = new URL(url, current);
+
+        const sku = current.searchParams.get("sku");
+        const skuv = current.searchParams.get("sku_variation");
+
+        // Preserves sku and sku_variation in the destination URL
+        if (sku) dest.searchParams.set("sku", sku);
+        if (skuv) dest.searchParams.set("sku_variation", skuv);
+
+        window.location.assign(dest);
+      });
+    }
+
+    if (publishBtn) {
+      publishBtn.addEventListener("click", () => {
+        alert(
+          "Your configuration will now be reviewed and then approved. " +
+          "This page is currently under construction."
+        );
+      });
+    }
+  }
 }
 
 /* Boot */
