d5b389b2beec360296fad103b754cca38ce6ad28
Aleina Rossui <admin@promoflow.net>
2026-04-13 07:14:31 -0400

    mensaje

--- view/preview_porduct/preview_porduct/preview_logic.js
+++ view/preview_porduct/preview_porduct/preview_logic.js
@@ -873,7 +873,7 @@ class PreviewLogic {
     );
     const max_quantity = this.getMaxQuantity();
 
-
+    alert("Bueno" + max_quantity);
     const url = "../../controller/order/product.php";
     const data = {
       action: "get_variation_prices",
