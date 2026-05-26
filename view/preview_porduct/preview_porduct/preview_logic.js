78a40f9def71c69c25baef7244abb50018020d3a
Aleina Rossui <admin@promoflow.net>
2026-05-26 17:37:00 -0400

    “…”

--- view/preview_porduct/preview_porduct/preview_logic.js
+++ view/preview_porduct/preview_porduct/preview_logic.js
@@ -849,7 +849,19 @@ class PreviewLogic {
     }
 
 
-    const var_label_quantity = document.getElementById("var_label_quantity");
+    const bb_unit = document.getElementById("bb_unit");
+    const bb_unit_quantity = document.getElementById("bb_unit_quantity");
+    const bb_extra_unit = document.getElementById("bb_extra_unit");
+    const bb_total = document.getElementById("bb_total");
+
+
+    bb_unit.innerHTML =  "£" + price;
+    bb_unit_quantity.innerHTML = quantity;
+    bb_extra_unit.innerHTML = "£" + totalExtraPrice;
+    bb_total.innerHTML = "";
+
+
+
 
   }
 
