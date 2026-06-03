d9c9868994c87a4d1118e21595de276c7c7bdb8d
Aleina Rossui <admin@promoflow.net>
2026-06-02 20:43:23 -0400

    “…”

--- controller/products/product.php
+++ controller/products/product.php
@@ -262,6 +262,9 @@ class Product {
 include "../../controller/config/database.php";
 include "../../model/products.php";
 include "../../controller/products/variations.php";
+include "../../controller/emails/send_emails.php";
+
+
 
 $productClass = new Product(); // instancia
 $productClass->handleProduct();
