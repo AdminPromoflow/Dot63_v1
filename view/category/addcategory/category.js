13404727beee2110b8575d4eefac5f581aea00e8
Aleina Rossui <admin@promoflow.net>
2026-06-18 02:00:31 -0400

    “…”

--- view/category/addcategory/category.js
+++ view/category/addcategory/category.js
@@ -204,7 +204,7 @@ class ClassCategory {
           classCategory.drawListCategories(data);
         }
         else if (mode == "edit") {
-          classCategory.drawCategorySelected(data['category_selected']);
+          classCategory.drawListCategories(data['category_selected']);
         }
       })
       .catch(error => {
