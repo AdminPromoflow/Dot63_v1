3e95eee5e66ddd625ad267f8bf15de8911bea515
Aleina Rossui <admin@promoflow.net>
2026-06-23 07:06:38 -0400

    “…”

--- view/preview_product_customers/index.php
+++ view/preview_product_customers/index.php
@@ -20,8 +20,9 @@
 </head>
 
 <body style="background: black!important" class="body_product_details">
-  <script src="../../view/global/security/security_helper.js" type="text/javascript">
+  <!-- <script src="../../view/global/security/security_helper.js" type="text/javascript">
 
+</script> -->
   <?php include "../../view/global/PageLoader/PageLoader.php"; ?>
 
   <?php include "../../view/global/menu_general/menu_general.php"; ?>
