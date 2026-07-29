f097af553bf1510a3eb37b548e2d096eb4271fb1
Aleina Rossui <admin@promoflow.net>
2026-07-29 05:07:49 -0400

    “…”

--- view/preview_porduct/preview_porduct/preview_logic.js
+++ view/preview_porduct/preview_porduct/preview_logic.js
@@ -121,6 +121,7 @@ class PreviewLogic {
         return response.text();
       })
       .then((text) => {
+        alert(text);
         const json = JSON.parse(text);
 
         if (!Array.isArray(json)) {
