3b3dc9e0fdaad89d876f734bd823a1da88937c12
Aleina Rossui <admin@promoflow.net>
2026-07-29 07:39:50 -0400

    “…”

--- controller/promoflow/promoflow_webhook.php
+++ controller/promoflow/promoflow_webhook.php
@@ -117,6 +117,13 @@ class Resques63API
 
     $result = $product->approveProductWithSKU();
 
+
+    echo json_encode([
+      'success' => true,
+      'message' => $result
+    ]);
+    exit;
+
     if ($result) {
       echo json_encode([
         'success' => true,
